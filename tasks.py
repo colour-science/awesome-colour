6225b1d1bd5c1886f8fa7e48770b1315d91a6e48
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-02-12 19:32:34 +1300

    Modernise development process.

--- tasks.py
+++ tasks.py
@@ -1,111 +1,72 @@
-# -*- coding: utf-8 -*-
 """
 Invoke - Tasks
 ==============
 """
 
-from __future__ import unicode_literals
 
 from invoke import task
 
-__author__ = 'Colour Developers'
-__copyright__ = 'Copyright 2018 Colour Developers'
-__license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
-__maintainer__ = 'Colour Developers'
-__email__ = 'colour-developers@colour-science.org'
-__status__ = 'Production'
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2018 Colour Developers"
+__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
 
-__all__ = ['formatting', 'quality', 'build', 'release']
-
-
-@task
-def formatting(ctx, yapf=True):
-    """
-    Formats the codebase with *Yapf*.
-
-    Parameters
-    ----------
-    ctx : invoke.context.Context
-        Context.
-    yapf : bool, optional
-        Whether to format the codebase with *Yapf*.
-
-    Returns
-    -------
-    bool
-        Task success.
-    """
-
-    if yapf:
-        print('Formatting codebase with "Yapf"...')
-        ctx.run('yapf -p -i -r --exclude \'.git\' .')
+__all__ = [
+    "precommit",
+    "build",
+    "release",
+]
 
 
 @task
-def quality(ctx, flake8=True):
+def precommit(ctx):
     """
-    Checks the codebase with *Flake8* and lints various *restructuredText*
-    files with *rst-lint*.
+    Run the "pre-commit" hooks on the codebase.
 
     Parameters
     ----------
-    ctx : invoke.context.Context
+    ctx
         Context.
-    flake8 : bool, optional
-        Whether to check the codebase with *Flake8*.
-
-    Returns
-    -------
-    bool
-        Task success.
     """
 
-    if flake8:
-        print('Checking codebase with "Flake8"...')
-        ctx.run('flake8')
+    print('Running "pre-commit" hooks on the codebase...')
+    ctx.run("pre-commit run --all-files")
 
 
-@task(formatting, quality)
+@task(precommit)
 def build(ctx):
     """
-    Builds the project.
+    Build the project.
 
     Parameters
     ----------
     ctx : invoke.context.Context
         Context.
-
-    Returns
-    -------
-    bool
-        Task success.
     """
 
-    print('Building...')
-    ctx.run('cp readme.md docs/index.md')
-    ctx.run('mkdocs build')
+    print("Building...")
+    ctx.run("cp readme.md docs/index.md")
+    ctx.run("mkdocs build")
 
 
 @task(build)
 def release(ctx):
     """
-    Releases the project to *Github Pages*.
+    Release the project to *Github Pages*.
 
     Parameters
     ----------
     ctx : invoke.context.Context
         Context.
-
-    Returns
-    -------
-    bool
-        Task success.
     """
 
-    print('Releasing...')
+    print("Releasing...")
 
-    output = ctx.run('git status')
-    assert 'nothing to commit, working tree clean' in output.stdout, (
-        'Working tree is not clean, please commit your changes!')
+    output = ctx.run("git status")
+    assert (
+        "nothing to commit, working tree clean" in output.stdout
+    ), "Working tree is not clean, please commit your changes!"
 
-    ctx.run('mkdocs gh-deploy')
+    ctx.run("mkdocs gh-deploy")
