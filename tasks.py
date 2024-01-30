4e5419caa90297eefada59e7a679470e17687724
Thomas Mansencal <thomas.mansencal@gmail.com>
2024-01-30 21:29:28 +1300

    Improve license reference consistency.

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ from invoke import task
 
 __author__ = "Colour Developers"
 __copyright__ = "Copyright 2018 Colour Developers"
-__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__license__ = "BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause"
 __maintainer__ = "Colour Developers"
 __email__ = "colour-developers@colour-science.org"
 __status__ = "Production"
